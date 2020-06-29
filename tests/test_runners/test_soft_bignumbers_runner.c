69b9ad9ae9e090e55a13d548053257382aac57f8
Pierre-Henry Moussay <pierre-henry.moussay@sifive.com>
2020-06-29 16:50:24 +0200

    Add test on big integer inc and move test on big integer compare
    Signed-off-by: Pierre-Henry Moussay <pierre-henry.moussay@sifive.com>

--- tests/test_runners/test_soft_bignumbers_runner.c
+++ tests/test_runners/test_soft_bignumbers_runner.c
@@ -24,4 +24,20 @@ TEST_GROUP_RUNNER(soft_bignumbers)
     RUN_TEST_CASE(soft_bignumbers, soft_bignum_sub_size_5);
     RUN_TEST_CASE(soft_bignumbers, soft_bignum_sub_size_5_with_carry);
     RUN_TEST_CASE(soft_bignumbers, soft_bignum_sub_size_5_with_carry_2);
+
+    /* increment by one */
+    RUN_TEST_CASE(soft_bignumbers, soft_bignum_inc_size_0);
+    RUN_TEST_CASE(soft_bignumbers, soft_bignum_inc_size_1);
+    RUN_TEST_CASE(soft_bignumbers, soft_bignum_inc_size_1_with_carry);
+    RUN_TEST_CASE(soft_bignumbers, soft_bignum_inc_size_2);
+    RUN_TEST_CASE(soft_bignumbers, soft_bignum_inc_size_2_with_carry);
+    RUN_TEST_CASE(soft_bignumbers, soft_bignum_inc_size_5);
+    RUN_TEST_CASE(soft_bignumbers, soft_bignum_inc_size_5_with_carry);
+
+    /* test compare */
+    RUN_TEST_CASE(soft_bignumbers, soft_bignum_compare_a_equals_b);
+    RUN_TEST_CASE(soft_bignumbers, soft_bignum_compare_a_greater_than_b_lsb);
+    RUN_TEST_CASE(soft_bignumbers, soft_bignum_compare_a_greater_than_b_msb);
+    RUN_TEST_CASE(soft_bignumbers, soft_bignum_compare_a_lower_than_b_lsb);
+    RUN_TEST_CASE(soft_bignumbers, soft_bignum_compare_a_lower_than_b_msb);
 }
--- tests/test_runners/test_utils_runner.c
+++ tests/test_runners/test_utils_runner.c
@@ -5,9 +5,4 @@ TEST_GROUP_RUNNER(utils)
 {
     RUN_TEST_CASE(utils, memcpy_u64_success);
     RUN_TEST_CASE(utils, memset_u64_success);
-    RUN_TEST_CASE(utils, memcmp_u64_a_equals_b);
-    RUN_TEST_CASE(utils, memcmp_u64_a_greater_than_b_lsb);
-    RUN_TEST_CASE(utils, memcmp_u64_a_greater_than_b_msb);
-    RUN_TEST_CASE(utils, memcmp_u64_a_lower_than_b_lsb);
-    RUN_TEST_CASE(utils, memcmp_u64_a_lower_than_b_msb);
 }
